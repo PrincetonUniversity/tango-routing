2558ad26e291f7bf0ac959091418228ff9c571a9
benherber <69871872+benherber@users.noreply.github.com>
2023-03-20 22:51:39 -0400

    fixed metric gathering

--- test/test_metric_collection.py
+++ test/test_metric_collection.py
@@ -3,7 +3,6 @@
 from test.models.events import (
     ArrayName,
     ArraySet,
-    ArraySetRange,
     IncomingTangoTraffic,
 )
 from test.models.interpreter import ExpectContains, TestCase, TestEvent, TestRunner
@@ -23,7 +22,7 @@ from edu.princeton.tango.mappers.tunnel_header_mapper import (
 )
 
 
-def test_seq_num_sig_check_multipath() -> None:
+def test_seq_num_metrics_multipath() -> None:
     """Test several paths verify sequence number signatures correctly."""
     given_timeout = 20000000
 
@@ -36,8 +35,8 @@ def test_seq_num_sig_check_multipath() -> None:
     )
 
     given_packets = []
-    given_num_packets = 64
-    for path in [0]:
+    given_num_packets = 65
+    for path in range(0, 8):
         given_packets = [
             *given_packets,
             *[
@@ -73,25 +72,79 @@ def test_seq_num_sig_check_multipath() -> None:
         class_mapper=given_traffic_mapping,
         header_mapper=given_header_mapping,
     ) as when:
-        print(when.run())
-        # when.run().expect().then(
-        #     ExpectContains("invalid_pkt_manager_0(65) : [7u32]"),
-        # ).then(
-        #     ExpectContains("invalid_pkt_manager_1(66) : [6u32]"),
-        # ).then(
-        #     ExpectContains("invalid_pkt_manager_2(67) : [5u32]"),
-        # ).then(
-        #     ExpectContains("invalid_pkt_manager_3(68) : [4u32]"),
-        # ).then(
-        #     ExpectContains("invalid_pkt_manager_4(69) : [3u32]"),
-        # ).then(
-        #     ExpectContains("invalid_pkt_manager_5(70) : [2u32]"),
-        # ).then(
-        #     ExpectContains("invalid_pkt_manager_6(71) : [1u32]"),
-        # ).then(
-        #     ExpectContains("invalid_pkt_manager_7(72) : [0u32]"),
-        # ).finish()
+        when.run().expect().then(
+            ExpectContains(
+                "".join(
+                    (
+                        "loss_metrics_manager_10(56) :",
+                        " [0u32; 126u32; 189u32; 252u32; 315u32; 378u32; 441u32; 504u32]",
+                    ),
+                ),
+            ),
+        ).finish()
+
+
+def test_delay_metrics_multipath() -> None:
+    """Test several paths verify sequence number signatures correctly."""
+    given_timeout = 295_000_000
+
+    given_traffic_mapping = ConfiguredTrafficClassMapper(
+        [FuzzyClassMapping(FuzzyFiveTuple(src_port=x), x - 1) for x in range(1, 9)],
+    )
+
+    given_header_mapping = ConfiguredHeaderMapper(
+        [TunnelHeader(x, IPv6HeaderMap(0, 0, x, 0, 0, 0, 0, 0)) for x in range(0, 8)],
+    )
+
+    given_packets = []
+    given_num_packets = 33
+    for path in range(0, 8):
+        given_packets = [
+            *given_packets,
+            *[
+                TestEvent(
+                    IncomingTangoTraffic(
+                        EthernetHeader(x, 0, 0),
+                        IPv6Header(0, 0, 0, 0, 0, 0, 0, 0),
+                        TangoHeader(path, ts_out, 0, 0, 0),
+                        IPv4Header(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                        FiveTuple(0, 0, path + 1, 0, 0),
+                    ),
+                    timestamp=ts_in,
+                )
+                for x, ts_out, ts_in in zip(
+                    range(1, 1 + given_num_packets),
+                    [(1_100_000 * x) + 1 for x in range(0, given_num_packets)],
+                    [(1_100_000 * x * path) + 1_100_001 for x in range(0, given_num_packets)],
+                    strict=True,
+                )
+            ],
+        ]
+
+    given_routes = [
+        TestEvent(ArraySet(ArrayName.ROUTE_MAPPINGS, x, x), timestamp=0) for x in range(0, 8)
+    ]
+
+    given_events = [*given_routes, *given_packets]
+
+    given_case = TestCase(given_timeout, given_events)
+
+    with TestRunner(
+        given_case,
+        class_mapper=given_traffic_mapping,
+        header_mapper=given_header_mapping,
+    ) as when:
+        when.run().expect().then(
+            ExpectContains(
+                "".join(
+                    (
+                        "delay_metrics_manager_9(66) : [59136u32; 61436u32;",
+                        " 61955u32; 62475u32; 62995u32; 63515u32; 64035u32; 64556u32]",
+                    ),
+                ),
+            ),
+        ).finish()
 
 
 if __name__ == "__main__":
-    test_seq_num_sig_check_multipath()
+    test_delay_sig_check_multipath()
