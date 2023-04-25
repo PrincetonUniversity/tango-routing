ab5d341c925e9b1b25b50c28e1d524b4dd522944
benherber <69871872+benherber@users.noreply.github.com>
2023-04-25 14:46:22 -0400

    created pcap send script

---
+++ src/python/edu/princeton/tango/controlplane/send_pcap.py
@@ -0,0 +1,22 @@
+"""Send precomputed signature pcap to switch."""
+
+import sys
+
+from scapy.sendrecv import sendp
+from scapy.utils import rdpcap
+
+
+def main() -> None:
+    """Send out pcap."""
+    if len(sys.argv) != 2:
+        print("Usage: <filename>")  # noqa: T201
+        sys.exit(-1)
+
+    filename = sys.argv[1]
+    pkts = rdpcap(filename)
+
+    sendp(pkts, iface="enp134s0f1")
+
+if __name__ == "__main__":
+    main()
+
