4ca318bf8297332637d58496529fee661631b2c2
benherber <69871872+benherber@users.noreply.github.com>
2023-04-24 22:54:40 -0400

    fixed kwargs

--- src/python/edu/princeton/tango/controlplane/populate_sigs_smoketest.py
+++ src/python/edu/princeton/tango/controlplane/populate_sigs_smoketest.py
@@ -36,7 +36,7 @@ class Signature(Packet):
     """Single signature payload of packet."""
 
     name = "Signature"
-    fields_desc = [IntField("Signature", 0)]
+    fields_desc = [IntField("signature", 0)]
 
 
 def main() -> None:
@@ -47,80 +47,80 @@ def main() -> None:
         Ether()
         / IPv6()
         / UDP()
-        / SignatureMetadata(SignatureType.SEQNUM_SIG.value, 0, 0)
-        / Signature(1)
-        / Signature(0),
+        / SignatureMetadata(sig_type=SignatureType.SEQNUM_SIG.value, sig_idx=0, block_idx=0)
+        / Signature(signature=1)
+        / Signature(signature=0),
     )
 
     pkts.append(
         Ether()
         / IPv6()
         / UDP()
-        / SignatureMetadata(SignatureType.SEQNUM_SIG.value, 1, 0)
-        / Signature(2)
-        / Signature(3)
-        / Signature(4)
-        / Signature(0),
+        / SignatureMetadata(sig_type=SignatureType.SEQNUM_SIG.value, sig_idx=1, block_idx=0)
+        / Signature(signature=2)
+        / Signature(signature=3)
+        / Signature(signature=4)
+        / Signature(signature=0),
     )
 
     pkts.append(
         Ether()
         / IPv6()
         / UDP()
-        / SignatureMetadata(SignatureType.SEQNUM_SIG.value, 0, 1)
-        / Signature(5)
-        / Signature(0),
+        / SignatureMetadata(sig_type=SignatureType.SEQNUM_SIG.value, sig_idx=0, block_idx=1)
+        / Signature(signature=5)
+        / Signature(signature=0),
     )
 
     pkts.append(
         Ether()
         / IPv6()
         / UDP()
-        / SignatureMetadata(SignatureType.SEQNUM_SIG.value, 1, 1)
-        / Signature(6)
-        / Signature(7)
-        / Signature(8)
-        / Signature(0),
+        / SignatureMetadata(sig_type=SignatureType.SEQNUM_SIG.value, sig_idx=1, block_idx=1)
+        / Signature(signature=6)
+        / Signature(signature=7)
+        / Signature(signature=8)
+        / Signature(signature=0),
     )
 
     pkts.append(
         Ether()
         / IPv6()
         / UDP()
-        / SignatureMetadata(SignatureType.TS_SIG.value, 0, 0)
-        / Signature(1)
-        / Signature(0),
+        / SignatureMetadata(sig_type=SignatureType.TS_SIG.value, sig_idx=0, block_idx=0)
+        / Signature(signature=1)
+        / Signature(signature=0),
     )
 
     pkts.append(
         Ether()
         / IPv6()
         / UDP()
-        / SignatureMetadata(SignatureType.TS_SIG.value, 1, 0)
-        / Signature(2)
-        / Signature(3)
-        / Signature(4)
-        / Signature(0),
+        / SignatureMetadata(sig_type=SignatureType.TS_SIG.value, sig_idx=1, block_idx=0)
+        / Signature(signature=2)
+        / Signature(signature=3)
+        / Signature(signature=4)
+        / Signature(signature=0),
     )
 
     pkts.append(
         Ether()
         / IPv6()
         / UDP()
-        / SignatureMetadata(SignatureType.TS_SIG.value, 0, 1)
-        / Signature(5)
-        / Signature(0),
+        / SignatureMetadata(sig_type=SignatureType.TS_SIG.value, sig_idx=0, block_idx=1)
+        / Signature(signature=5)
+        / Signature(signature=0),
     )
 
     pkts.append(
         Ether()
         / IPv6()
         / UDP()
-        / SignatureMetadata(SignatureType.TS_SIG.value, 1, 1)
-        / Signature(6)
-        / Signature(7)
-        / Signature(8)
-        / Signature(0),
+        / SignatureMetadata(sig_type=SignatureType.TS_SIG.value, sig_idx=1, block_idx=1)
+        / Signature(signature=6)
+        / Signature(signature=7)
+        / Signature(signature=8)
+        / Signature(signature=0),
     )
 
     sendp(pkts, iface="enp134s0f1")
